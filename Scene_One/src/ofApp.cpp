dd89bcdd7d9c69d288dc37e131fd23891df685ec
toshiemon18 <st12318@gmail.com>
2014-05-04 16:54:22 +0900

    三角形と円の運動変えてみた

--- Scene_One/src/ofApp.cpp
+++ Scene_One/src/ofApp.cpp
@@ -1,19 +1,19 @@
 #include "ofApp.h"
 #include "Drawfigure.h"
 
-#define OUTLINE 3.5
+#define OUTLINE 2
 #define N 3
 
 Drawfigure circle, triangle, judg;
 float colorparam[4][3] = { {255, 206, 10}, {131, 255, 0}, {20, 246, 255}, {250, 0, 191} };
 
 //oneCircle declaration
-float xCircleLocation, yCircleLocation;
-float CircleAccelx, CircleAccely;
+float xCircleLocation, xCircleLocation2, yCircleLocation, yCircleLocation2;
+float CircleAccelx, CircleAccelx2, CircleAccely, CircleAccely2;
 
 //oneTriangle declaration
-float xTriangleLocation[3] = {70, 55, 75};
-float yTriangleLocation[3] = {20, 40, 38};
+float xTriangleLocation[2][3];
+float yTriangleLocation[2][3];
 float TriangleAccelx[3];
 float TriangleAccely[3];
 
@@ -28,15 +28,19 @@ void ofApp::setup(){
 	ofEnableSmoothing();
 
 	//oneCircle setup
-	xCircleLocation = ofGetWidth() / 4.0;
-	yCircleLocation = ofGetHeight() / 3.0;
+	xCircleLocation = ofGetWidth() / 2.0;
+	yCircleLocation = ofGetHeight() / 2.0;
 
-	CircleAccelx = 55;
-	CircleAccely = 60;
+	CircleAccelx = 10;
+	CircleAccely = 25;
+	CircleAccelx2 = CircleAccely;
+	CircleAccely2 = CircleAccelx;
 
 	//oneTriangle setup
 	for (int i = 0; i < N; i++){
-		TriangleAccelx[i] = 60 - (i + 1) * 10;
+		xTriangleLocation[0][i] = ofGetWidth() / 2.0;
+		TriangleAccelx[i] = 40 - (i + 1) * 10 ;
+		yTriangleLocation[0][i] = ofGetHeight() / 2.0;
 		TriangleAccely[i] = 20 + (i + 1) * 10;
 	}
 	
@@ -45,24 +49,32 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 
+	ofBackground(0);
+
 	//oneCircle update
 	xCircleLocation += CircleAccelx;
+	xCircleLocation2 += CircleAccelx2;
 	yCircleLocation += CircleAccely;
+	yCircleLocation2 += CircleAccely2;
 
 	CircleAccelx *= judg.judgment(xCircleLocation, 'x');
+	CircleAccelx2 *= judg.judgment(xCircleLocation2, 'x');
 	CircleAccely *= judg.judgment(yCircleLocation, 'y');
-
+	CircleAccely2 *= judg.judgment(yCircleLocation2, 'y');
 
 	//oneTriangle update
 	for (int i = 0; i < N; i++){
 
-		xTriangleLocation[i] += TriangleAccelx[i];
-		yTriangleLocation[i] += TriangleAccely[i];
+		xTriangleLocation[0][i] += TriangleAccelx[i];
+		yTriangleLocation[0][i] += TriangleAccely[i];
+		xTriangleLocation[1][i] = (ofGetWidth() / 2.0 - xTriangleLocation[0][i]) * 2 + xTriangleLocation[0][i];
+		yTriangleLocation[1][i] = (ofGetHeight() / 2.0 - yTriangleLocation[0][i]) * 2 + yTriangleLocation[0][i];
 
-		TriangleAccelx[i] *= judg.judgment(xTriangleLocation[i], 'x');
-		TriangleAccely[i] *= judg.judgment(yTriangleLocation[i], 'y');
+		TriangleAccelx[i] *= judg.judgment(xTriangleLocation[0][i], 'x');
+		TriangleAccely[i] *= judg.judgment(yTriangleLocation[0][i], 'y');
 
 	}
+
 
 }	
 
@@ -70,12 +82,14 @@ void ofApp::update(){
 void ofApp::draw(){
 
 	//oneCircle drawing
-	circle.oneCircle(xCircleLocation, yCircleLocation, 50 + rand()%50, OUTLINE + 0.5, colorparam[rand() % 4]);
-	circle.oneCircle(yCircleLocation, xCircleLocation, 50 + rand()%50, OUTLINE + 0.5, colorparam[rand() % 4]);
+	circle.oneCircle(xCircleLocation, yCircleLocation, 70, OUTLINE + 0.5, colorparam[rand() % 4]);
+	circle.oneCircle((ofGetWidth() / 2.0 - xCircleLocation) * 2 + xCircleLocation, (ofGetHeight() / 2.0 - yCircleLocation) * 2 + yCircleLocation, 80, OUTLINE + 0.5, colorparam[rand() % 4]);
+	circle.oneCircle(xCircleLocation2, yCircleLocation2, 70, OUTLINE + 0.5, colorparam[rand() % 4]);
+	circle.oneCircle((ofGetWidth() / 2.0 - xCircleLocation2) * 2 + xCircleLocation2, (ofGetHeight() / 2.0 - yCircleLocation2) * 2 + yCircleLocation2, 90, OUTLINE + 0.5, colorparam[rand() % 4]);
 
 	//oneTriangle drawing
-	triangle.oneTriangle(xTriangleLocation, yTriangleLocation, OUTLINE, colorparam[rand() % 4]);
-	triangle.oneTriangle(yTriangleLocation, xTriangleLocation, OUTLINE, colorparam[rand() % 4]);
+	triangle.oneTriangle(xTriangleLocation[0], yTriangleLocation[0], OUTLINE, colorparam[rand() % 4]);
+	triangle.oneTriangle(xTriangleLocation[1], yTriangleLocation[1], OUTLINE, colorparam[rand() % 4]);
 
 }
 
