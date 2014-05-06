c8e2ffbbcc852e93705f80c802356e322e77f183
toshiemon18 <st12318@gmail.com>
2014-05-06 22:42:01 +0900

    Intro

--- src/Drawfigure.cpp
+++ src/Drawfigure.cpp
@@ -1,67 +1,72 @@
 #include "ofMain.h"
 #include "Drawfigure.h"
 
-void Drawfigure::oneCircle(float x, float y, float diameter, float diff, float *color) {
-    
-	ofSetColor(color[0], color[1], color[2]);
-	ofNoFill();
-	ofSetLineWidth(diff);
-	ofCircle(x, y, diameter);
-    
+void Drawfigure::drawCircle(float x, float y, float diameter, float diff, float *color, int switcher) {
+
+	switch(switcher){
+	case 0:
+		ofSetColor(color[0], color[1], color[2]);
+		ofNoFill();
+		ofSetLineWidth(diff);
+		ofCircle(x, y, diameter);
+		break;
+
+	case 1:
+		ofSetColor(color[0], color[1], color[2]);
+		ofFill();
+		ofCircle(x, y, diameter);
+		break;
+	}
+
 }
 
-void Drawfigure::oneTriangle(float *x, float *y, float diff, float *color) {
-    
-	ofSetColor(color[0], color[1], color[2]);
-	ofNoFill();
-	ofSetLineWidth(diff);
-    
-	ofTriangle(x[0], y[0], x[1], y[1], x[2], y[2]);
-    
-}
+void Drawfigure::drawTriangle(float *x, float *y, float diff, float *color, int switcher) {
 
-void Drawfigure::oneTrapezoid(float *x, float *y, float diff, float *color) {
-    
-	ofSetColor(color[0], color[1], color[2]);
-	ofNoFill();
-	ofSetLineWidth(diff);
-    
-	ofBeginShape();
-    ofVertex(x[0], y[0]);
-    ofVertex(x[1], y[1]);
-    ofVertex(x[2], y[2]);
-    ofVertex(x[3], y[3]);
-	ofEndShape();
-    
+	switch(switcher){
+	case 0:
+		ofSetColor(color[0], color[1], color[2]);
+		ofNoFill();
+		ofSetLineWidth(diff);
+		ofTriangle(x[0], y[0], x[1], y[1], x[2], y[2]);
+		break;
+
+	case 1:
+		ofSetColor(color[0], color[1], color[2]);
+		ofFill();
+		ofTriangle(x[0], y[0], x[1], y[1], x[2], y[2]);
+		break;
+	}
+
 }
 
 int frag = 0;
 
-float Drawfigure::judgment(float location, char xy) {
-    
+float Drawfigure::locationJudgment(float location, char xy) {
+
 	switch(xy){
 		case 'x':
-            
+
 			if (location < 0 || location > ofGetWidth()) {
 				frag = -1;
 			}
-            
+
 			else{
 				frag = 1;
 			}
 			break;
-            
+
 		case 'y':
-            
+
 			if (location < 0 || location > ofGetHeight()) {
 				frag = -1;
 			}
-            
+
 			else{
 				frag = 1;
 			}
 			break;
 	}
-    
+
 	return frag;
+
 }
---
+++ src/Intro.cpp
@@ -0,0 +1,60 @@
+#include "ofApp.h"
+#include "Drawfigure.h"
+
+#define OUTLINE 3
+#define PARTICLES 30
+
+Drawfigure judg;
+float colorparam[4][3] = { {255, 206, 10}, {131, 255, 0}, {20, 246, 255}, {250, 0, 191} };
+
+//oneCircle declaration
+Drawfigure circle;
+ofVec2f CirclePositionVector[PARTICLES];
+ofVec2f CircleDirectionVector[PARTICLES];
+int diameter[PARTICLES];
+int pattern[PARTICLES];
+float t = 1;
+
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+
+	ofBackground(255);
+	ofSetCircleResolution(64);
+	ofEnableSmoothing();
+
+	//oneCircle setup
+	for(int i = 0; i < PARTICLES; i++){
+		CirclePositionVector[i].x = ofRandom(ofGetWidth());
+		CirclePositionVector[i].y = ofRandom(ofGetHeight());
+		CircleDirectionVector[i].x = ofRandom(-20, 20);
+		CircleDirectionVector[i].y = ofRandom(-20, 20);
+		diameter[i] = ofRandom(20, 80);
+		pattern[i] = ofRandom(0, 2);
+	}
+	
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+
+	//oneCircle update
+	for(int i = 0; i < PARTICLES; i++){
+		CirclePositionVector[i].x += CircleDirectionVector[i].x;
+		CirclePositionVector[i].y += CircleDirectionVector[i].y;
+		CircleDirectionVector[i].x *=judg.locationJudgment(CirclePositionVector[i].x, 'x');
+		CircleDirectionVector[i].y *=judg.locationJudgment(CirclePositionVector[i].y, 'y');
+	}
+	
+}	
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	
+	//oneCircle drawing
+	for(int i = 0; i < PARTICLES; i++){
+		circle.drawCircle(CirclePositionVector[i].x, CirclePositionVector[i].y, diameter[i], OUTLINE, colorparam[i % 4], pattern[i]);
+	}
+
+}
+
