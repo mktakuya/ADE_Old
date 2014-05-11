0bc70fb03f9e25994401f3ea3997eda97a1403cd
toshiemon18 <st12318@gmail.com>
2014-05-11 16:58:23 +0900

    三角形の表示と色を改善

--- src/Triangles.cpp
+++ src/Triangles.cpp
@@ -1,13 +1,13 @@
 #include "ofApp.h"
 #include "Drawfigure.h"
 
-#define OUTLINE 3
+#define OUTLINE 2.5
 #define PIECES 120
 #define XBOADER 600
 #define YBOADER 300
 
 //Decralation color parameters
-float colorparam[4][3] = { {255, 206, 10}, {131, 255, 0}, {20, 246, 255}, {250, 0, 191} };
+float colorparam[4][3] = { {193, 186, 186}, {214, 214, 214}, {227, 230, 232}, {202, 205, 206} };
 
 //Decralation Drawfigure instance to use locationJudgment2
 Drawfigure judg;
@@ -18,6 +18,7 @@ ofVec2f TrianglePositionVector[PIECES][3];
 ofVec2f TriangleDirectionVector[PIECES];
 float gravity, friction;
 int pattern[PIECES];
+int alpha[PIECES];
 
 
 //--------------------------------------------------------------
@@ -33,10 +34,11 @@ void ofApp::setup(){
 	for(int cnt = 0; cnt < PIECES; cnt++){
 		for(int ct = 0; ct < 3; ct++){
 			TrianglePositionVector[cnt][ct].x = ofRandom(XBOADER, ofGetWidth() - XBOADER);
-			TrianglePositionVector[cnt][ct].y = ofRandom(YBOADER, ofGetHeight() - YBOADER);
+			TrianglePositionVector[cnt][ct].y = ofRandom(YBOADER, ofGetHeight() - YBOADER* 2);
 			TriangleDirectionVector[cnt].x = ofRandom(-10, 10);
 			TriangleDirectionVector[cnt].y = ofRandom(-10, 10);
 			pattern[cnt] = ofRandom(0, 2);
+			alpha[cnt] = ofRandom(45, 127);
 		}
 	}
 }
@@ -71,7 +73,7 @@ void ofApp::draw(){
 		ofTranslate(TriangleDirectionVector[i].x, TriangleDirectionVector[i].y);
 		triangle.drawTriangle(TrianglePositionVector[i][0].x, TrianglePositionVector[i][0].y, 
 			TrianglePositionVector[i][1].x, TrianglePositionVector[i][1].y, TrianglePositionVector[i][2].x,
-			TrianglePositionVector[i][2].y, OUTLINE, colorparam[i % 4], pattern[i], 127);
+			TrianglePositionVector[i][2].y, OUTLINE, colorparam[i % 4], pattern[i], alpha[i]);
 		ofPopMatrix();
 	}
 }
